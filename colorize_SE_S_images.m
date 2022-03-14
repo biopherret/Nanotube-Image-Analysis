9c84b335f9b753dfa26dffd2647ba46d83081ba3
sarahalexw <webster.sarah.a@gmail.com>
2022-03-13 22:34:30 -0500

    renamed the folders to RGB Stacks so that I can use the exact same nanotube finder code for them

--- colorize_SE_S_images.m
+++ colorize_SE_S_images.m
@@ -36,13 +36,13 @@ ColorImages = ParallelTable[ImageAdjust[ColorCombine[{Images[[i, j]], background
 
 Print["Exporting color images as tifs..."]
 (*Export color images to the RGB Stacks Folders as tif and jpg*)
-ParallelTable[Quiet[CreateDirectory[BaseFolders[[i]] <> "\\Colorized tif"]], {i, NumFolders}]
-ParallelTable[Quiet[CreateDirectory[BaseFolders[[i]] <> "\\Colorized jpg"]], {i, NumFolders}]
+ParallelTable[Quiet[CreateDirectory[BaseFolders[[i]] <> "\\RGB Stacks tif"]], {i, NumFolders}]
+ParallelTable[Quiet[CreateDirectory[BaseFolders[[i]] <> "\\RGB Stacks jpg"]], {i, NumFolders}]
 TotalImages = Sum[NumImages[[i]], {i, NumFolders}]
 Progress[x_] = N[x/TotalImages, 2]*100
 counter = 0
 SetSharedVariable[counter]
-ParallelTable[Export[BaseFolders[[i]] <> "\\Colorized tif\\" <> ImageBaseFileNames[[i, j]] <> ".tif", ColorImages[[i, j]]]; counter += 1; If[Mod[counter, 10] == 0, Print[ToString[Progress[counter]] <> "% of tif images exported"]], {i, NumFolders}, {j, NumImages[[i]]}]
+ParallelTable[Export[BaseFolders[[i]] <> "\\RGB Stacks tif\\" <> ImageBaseFileNames[[i, j]] <> ".tif", ColorImages[[i, j]]]; counter += 1; If[Mod[counter, 10] == 0, Print[ToString[Progress[counter]] <> "% of tif images exported"]], {i, NumFolders}, {j, NumImages[[i]]}]
 Print["Exporting color images as jpgs..."]
 counter = 0
-ParallelTable[Export[BaseFolders[[i]] <> "\\Colorized jpg\\" <> ImageBaseFileNames[[i, j]] <> ".jpg", ColorImages[[i, j]]]; counter += 1; If[Mod[counter, 10] == 0, Print[ToString[Progress[counter]] <> "% of jpg images exported"]], {i, NumFolders}, {j, NumImages[[i]]}]
+ParallelTable[Export[BaseFolders[[i]] <> "\\RGB Stacks jpg\\" <> ImageBaseFileNames[[i, j]] <> ".jpg", ColorImages[[i, j]]]; counter += 1; If[Mod[counter, 10] == 0, Print[ToString[Progress[counter]] <> "% of jpg images exported"]], {i, NumFolders}, {j, NumImages[[i]]}]
