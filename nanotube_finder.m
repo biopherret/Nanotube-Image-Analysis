5984af563aef49d060c4cb3b00e07a5a7b8d5509
sarahalexw <webster.sarah.a@gmail.com>
2022-03-16 12:25:50 -0500

    migrate colorize and rgb stacker stripts into nanotube_finedre

--- nanotube_finder.m
+++ nanotube_finder.m
@@ -7,8 +7,8 @@ Sidedness = ToString[Input["ts or os tubes?: "]]
 
 (*Hardcoded Inputs*)
 g = 2
-FinalMedian = 0.25
-FinalMax = 1.3
+FinalMedian = 0.22
+FinalMax = 1.6
 
 Print["Loading Files..."]
 BaseFolders = Flatten[FileNames[All, MainDirec]] (*find folders in the current directory*)
@@ -33,19 +33,16 @@ Print["Increacing contrast and croping images..."]
 AdjustParams = Quiet[Solve[(b + 1) (c + 1) d^g - c/2 == FinalMedian && (b + 1) (c + 1) m^g - c/2 == FinalMax, {b, c}]]
 b[d_, m_] = AdjustParams[[1,1,2]]
 c[d_, m_] = AdjustParams[[1,2,2]]
-Print["get raw images"]
+
 RAWImages = If[Sidedness == "os", (*load and crop images*)
     ParallelTable[ImageCrop[Import[Files[[i, j]]], {1354, 1030}, {Left, Bottom}], {i, NumFolders}, {j, NumImages[[i]]}],
     <|
     "green" -> ParallelTable[ImageCrop[Import[Files[["green", i, j]]], {1354, 1030}, {Left, Bottom}], {i, NumFolders}, {j, NumImages[[i]]}],
-    "blue" -> ParallelTable[ImageCrop[Import[Files[["blue", i, j]]], {2354, 1030}, {Left, Bottom}], {i, NumFolders}, {j, NumImages[[i]]}]
+    "blue" -> ParallelTable[ImageCrop[Import[Files[["blue", i, j]]], {1354, 1030}, {Right, Top}], {i, NumFolders}, {j, NumImages[[i]]}]
     |>]
-Print["get pixel value"]
 PixelValues = If[Sidedness == "os", (*find median and max pixel values of pre adjusted iamges*)
     ParallelTable[ImageMeasurements[RAWImages[[i, j]], {"Median", "Max"}], {i, NumFolders}, {j, NumImages[[i]]}],
     (ParallelTable[ImageMeasurements[#[[i]], {"Median", "Max"}], {i, NumFolders}]) &/@ RAWImages]
-Print["adjust images"]
-Print[PixelValues[["green", 1, 1]]]
 AdjustImages = If[Sidedness == "os",
     ParallelTable[ImageAdjust[RAWImages[[i,j]], {c[PixelValues[[i,j,1]], PixelValues[[i,j,2]]], b[PixelValues[[i,j,1]], PixelValues[[i,j,2]]], 2}], {i, NumFolders}, {j, NumImages[[i]]}],
     <|
@@ -53,4 +50,22 @@ AdjustImages = If[Sidedness == "os",
         "blue" -> ParallelTable[ImageAdjust[RAWImages[["blue",i,j]], {c[PixelValues[["blue", i,j,1]], PixelValues[["blue", i,j,2]]], b[PixelValues[["blue", i,j,1]], PixelValues[["blue", i,j,2]]], 2}], {i, NumFolders}, {j, NumImages[[i]]}]
     |>]
 
+Print["Creating composit images..."]
+Backgrd = Image[ConstantArray[FinalMedian, {1030, 1354}]] (*background to use to fill in channels for the os tubes*)
+
+ColorImages = If[Sidedness == "os",
+    ParallelTable[ColorCombine[{AdjustImages[[i, j]], Backgrd, 0.5*Backgrd + 0.5*AdjustImages[[i, j]]}, "RGB"], {i, NumFolders}, {j, NumImages[[i]]}],
+    ParallelTable[ColorCombine[{AdjustImages[["green", i, j]], AdjustImages[["blue", i, j]], 0.5*AdjustImages[["blue", i, j]] + 0.5*AdjustImages[["green", i, j]]}, "RGB"], {i, NumFolders}, {j, NumImages[[i]]}]]
+
+Print["Exporting color images as tifs..."]
+ParallelTable[Quiet[CreateDirectory[BaseFolders[[i]] <> "\\RGB Stacks tif"]], {i, NumFolders}]
+ParallelTable[Quiet[CreateDirectory[BaseFolders[[i]] <> "\\RGB Stacks jpg"]], {i, NumFolders}]
+TotalImages = Sum[NumImages[[i]], {i, NumFolders}]
+Progress[x_] = N[x/TotalImages, 2]*100
+counter = 0
+SetSharedVariable[counter]
+ParallelTable[Export[BaseFolders[[i]] <> "\\RGB Stacks tif\\" <> ImageBaseFileNames[[i, j]] <> ".tif", ColorImages[[i, j]]]; counter += 1; If[Mod[counter, 10] == 0, Print[ToString[Progress[counter]] <> "% of tif images exported"]], {i, NumFolders}, {j, NumImages[[i]]}]
+Print["Exporting color images as jpgs..."]
+counter = 0
+ParallelTable[Export[BaseFolders[[i]] <> "\\RGB Stacks jpg\\" <> ImageBaseFileNames[[i, j]] <> ".jpg", ColorImages[[i, j]]]; counter += 1; If[Mod[counter, 10] == 0, Print[ToString[Progress[counter]] <> "% of jpg images exported"]], {i, NumFolders}, {j, NumImages[[i]]}]
 
