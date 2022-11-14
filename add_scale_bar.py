391e11d651e338c67870ef6bbc6c9485431cfbe1
sarahalexw <webster.sarah.a@gmail.com>
2022-11-13 19:37:00 -0800

    added inputs so that you can unput the objcetive used and the length of the scale bar in microns you want

--- add_scale_bar.py
+++ add_scale_bar.py
@@ -7,13 +7,7 @@ import os
 #\Nanotube Image Analysis
 #   this file.py
 #   \Images
-#       \Date1
-#       \Date2
-#           \images folder
-#           \another images folder
-#               \RAW
-#                   image 1
-#                   image 2
+#      \some folder with images 
 
 #change and get current working directory (cwd)
 os.chdir('{}\Images\{}'.format(os.getcwd(), input('Input the directory path to folder of images to add scale bars too (from the Images Folder): ')))
@@ -27,6 +21,23 @@ cwd = os.getcwd()
 #make new directory
 new_folder = '{}\{} with scale bars'.format(cwd, os.path.basename(os.path.normpath(org_image_dir))) 
 os.mkdir(new_folder)
+
+objective = input('What objective was used?: ')
+if objective == '100x':
+    pixel_per_micron = 15.5
+elif objective == '60x':
+    pixel_per_micron = 9.3
+elif objective == '40x':
+    pixel_per_micron = 6.2
+elif objective == '20x':
+    pixel_per_micron = 3.1
+elif objective == '4x':
+    pixel_per_micron = 0.62
+else:
+    print(f'{objective} pixel to micron conversation not saved, att the calibration measurement to the script and re-run.')
+
+scalebar_len_um = int(input('What is the desired length of the scalebar(um)?: '))
+scalebar_len_pixels = int(scalebar_len_um * pixel_per_micron)
 
 for image_file_name in image_files:
     #create a 1354 x 1030 pixel, 72 dpi, blank figure with no frame
@@ -42,7 +53,7 @@ for image_file_name in image_files:
 
     #add the scale bar
     scalebar = AnchoredSizeBar(ax.transData,
-                       155, '10 \u03BCm', 'lower right', #pixel length of the bar, caption, position
+                       scalebar_len_pixels, f'{scalebar_len_um} \u03BCm', 'lower right', #pixel length of the bar, caption, position
                         pad=1,
                        color='white',
                        frameon=False,
