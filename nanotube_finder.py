69ae6bc2902f2a61bbff123322047c75540f9c41
sarahalexw <webster.sarah.a@gmail.com>
2023-01-08 12:33:28 -0800

    fixed allignment step by 2 pixels

--- nanotube_finder.py
+++ nanotube_finder.py
@@ -244,8 +244,8 @@ cwd = os.getcwd()
 new_folder = '{}\\Nanotube finder results'.format(image_dir)
 os.mkdir(new_folder)
 
-green_images = [plt.imread('{}\RAW\{}'.format(image_dir, image_file))[:1024,20:] for image_file in image_files[1::2]] #due to the way samples are imaged, the two images are slightly misaligned so we crop the excess of each
-blue_images = [plt.imread('{}\RAW\{}'.format(image_dir, image_file))[16:,:1356] for image_file in image_files[::2]] #[y-direction, x-direction]
+green_images = [plt.imread('{}\RAW\{}'.format(image_dir, image_file))[:1024,18:] for image_file in image_files[1::2]] #due to the way samples are imaged, the two images are slightly misaligned so we crop the excess of each
+blue_images = [plt.imread('{}\RAW\{}'.format(image_dir, image_file))[16:,:1358] for image_file in image_files[::2]] #[y-direction, x-direction]
 
 ydim, xdim = 1030, 1354 #images are 1354 x 1030 post crop
 num_images = len(green_images)
